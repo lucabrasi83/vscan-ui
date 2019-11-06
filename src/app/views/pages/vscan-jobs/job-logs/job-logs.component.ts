7847762768ac2d852dd357b8745d9186fa8766e4
lucabrasi83 <sebastien.pouplin@tatacommunications.com>
2019-11-06 07:46:29 +0700

    Various improvements on the Scan Jobs History component

---
+++ src/app/views/pages/vscan-jobs/job-logs/job-logs.component.ts
@@ -0,0 +1,42 @@
+import {
+	Component,
+	ElementRef,
+	Inject,
+	OnInit,
+	ViewChild
+} from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { ClipboardService } from "ngx-clipboard";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Component({
+	selector: "vscan-job-logs",
+	templateUrl: "./job-logs.component.html",
+	styleUrls: ["./job-logs.component.scss"]
+})
+export class JobLogsComponent implements OnInit {
+	constructor(
+		public dialogRef: MatDialogRef<JobLogsComponent>,
+		private _clipboardService: ClipboardService,
+		private _snackBar: MatSnackBar,
+		@Inject(MAT_DIALOG_DATA) public data: any
+	) {}
+
+	// Log Stream View
+	@ViewChild("logStreamContent", { static: false })
+	logStreamContent: ElementRef;
+
+	ngOnInit() {}
+	onCloseDialog() {
+		this.dialogRef.close();
+	}
+
+	onCopyLogContent() {
+		this._clipboardService.copyFromContent(
+			this.logStreamContent.nativeElement.textContent
+		);
+		this._snackBar.open("Logs copied to clipboard", "Dismiss", {
+			duration: 3000
+		});
+	}
+}
