064d216737b65efabfeeb1024c32a892b1562b28
lucabrasi83 <sebastien.pouplin@tatacommunications.com>
2020-01-28 19:25:26 +0700

    NG Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	isMockEnabled: false, // You have to switch this, when your real back-end is done
 	authTokenKey: "authce9d77b308c149d5992a80073637e4d5",
-	vscanAPIURL: "https://vulscano.vsnl.co.in:8443/api/v1",
-	vscanWebSocketURL: "wss://vulscano.vsnl.co.in:8443/api/v1",
+	vscanAPIURL: "https://vscan-back.maleeja.com/api/v1",
+	vscanWebSocketURL: "wss://vscan-back.maleeja.com/api/v1",
 	vscanJWT: "vscan_jwt"
 };
